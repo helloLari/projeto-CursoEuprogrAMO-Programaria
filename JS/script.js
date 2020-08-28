e4be225ac453abe2223bd7137fcdbe432c3d1f7a
larrisse <57159487+larrisse@users.noreply.github.com>
2020-08-28 00:49:52 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,10 @@
 document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
 function validaFormulario(){
-  if(document.getElementById("nome").value != "" &&    document.getElementById("email").value != "" && document.getElementById("telefone").value != ""){
-    alert("Prontinho! Você receberá as novidades por email!")
-  }else{
-    alert("Por favor, preencha os campos nome e email.")
-  }
+
+if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("tel").value != "" ){
+  alert("Prontinho! Voce receberá as novidades por e-mail.")
+}else{
+  alert("Por favor, preencha os campos nome, telefone e email")
 }
-
-const botao = document.getElementById("enviar");
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-
-enviar.addEventListener("click", () => {
-	if (nome.value != "" && email.value != ""){
-		alert("Cadastro enviado!");
-	} else {
-		alert("Preencha todos os campos do formulário!");
-	}
-});
+}
